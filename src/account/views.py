b00be467f2b02ae4b47cdd34c4a73c46832109da
vsrede <vsrede94@gmail.com>
2023-08-20 17:07:18 +0200

    added

--- src/account/views.py
+++ src/account/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from account.forms import UserRegistrationForm
+
+
+class UserRegistrationView(CreateView):
+    template_name = "user_create.html"
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy("index")
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+
+        send_registration_email(request=self.request, user_instance=user)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -110,3 +110,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = (BASE_DIR / "static",)
