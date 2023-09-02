4c131401209d697128af48a4be2d447771589b86
vsrede <vsrede94@gmail.com>
2023-09-02 21:23:40 +0200

    added docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM python:3.11-rc-slim
 
-RUN apt update & mkdir /fusionstyle
+RUN apt update && mkdir /fusionstyle
 
 WORKDIR /fusionstyle
 
 COPY ./src ./src ./\
-    ./requirements.txt ./requirements.txt ./
+    ./requirements.txt ./requirements.txt ./\
+    ./commands ./commands ./
 
 
-RUN python -m pip install --upgrade pip & pip install -r ./requirements.txt
+RUN python -m pip install --upgrade pip && pip install -r ./requirements.txt
 
-CMD ["python", "src/manage.py", "runserver", "8080"]
+CMD ["bash"]
--- src/config/settings/development.py
+++ src/config/settings/development.py
@@ -1,3 +1,5 @@
+import os
+
 from config.settings.base import *  # NOQA
 
 DEBUG = True
@@ -8,6 +10,14 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT")
+    },
+    "sqlite": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",  # NOQA
     }
