1385703eb3c4d093573a79f8ce1bebe236ff4379
vsrede <vsrede94@gmail.com>
2023-09-18 20:36:55 +0200

    delete commented code

--- src/config/settings/development.py
+++ src/config/settings/development.py
@@ -22,22 +22,22 @@ if os.environ.get("GITHUB_WORKFLOW"):
 else:
     DATABASES = {
         # local
-        # "default": {
-        #     "ENGINE": "django.db.backends.postgresql",
-        #     "NAME": "vladyslav_oriekhov",
-        #     "USER": "vladyslav",
-        #     "PASSWORD": "admin",
-        #     "HOST": "localhost",
-        #     "PORT": 5432,
-        # },
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB"),
-            "USER": os.environ.get("POSTGRES_USER"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-            "HOST": os.environ.get("POSTGRES_HOST"),
-            "PORT": os.environ.get("POSTGRES_PORT"),
-        }
+            "NAME": "vladyslav_oriekhov",
+            "USER": "vladyslav",
+            "PASSWORD": "admin",
+            "HOST": "localhost",
+            "PORT": 5432,
+        },
+        # "default": {
+        #     "ENGINE": "django.db.backends.postgresql",
+        #     "NAME": os.environ.get("POSTGRES_DB"),
+        #     "USER": os.environ.get("POSTGRES_USER"),
+        #     "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        #     "HOST": os.environ.get("POSTGRES_HOST"),
+        #     "PORT": os.environ.get("POSTGRES_PORT"),
+        # }
     }
 
 STATIC_ROOT = BASE_DIR / "static/"  # NOQA
