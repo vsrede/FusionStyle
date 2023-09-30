da3d99dd2032bcc79cdad1f60eb68ae46e64156c
vsrede <vsrede94@gmail.com>
2023-09-30 18:33:04 +0200

    nginx

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -22,8 +22,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",  # NOQA
     }
 }
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # NOQA
 STATIC_ROOT = BASE_DIR / "static_content/"
 STATIC_URL = "/static/"
 
