8fd11bf008148e56f6b5361855ec1cd90e1fb77e
vsrede <vsrede94@gmail.com>
2023-10-06 22:23:48 +0200

    fixed filter, delete all_products

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -3,13 +3,12 @@ from django.urls import path
 from shop.views import (AddFavoriteView, AddToCartView, CartView,
                         CreateOrderView, FavoriteListView, OrderDetailListView,
                         OrdersListView, ProductAllListView, ProductDetailView,
-                        ProductListView, RemoveFromCartView, UpdateCartView,
+                        RemoveFromCartView, UpdateCartView,
                         generate_instances_view, remove_favorite)
 
 app_name = "shop"
 
 urlpatterns = [
-    path("products/<str:category>/", ProductListView.as_view(), name="products_list"),
     path("products_all", ProductAllListView.as_view(), name="products_list_all"),
     path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
     path("add-to-cart/<int:product_id>/", AddToCartView.as_view(), name="add_to_cart"),
@@ -23,18 +22,6 @@ urlpatterns = [
     path("add/<int:product_id>/", AddFavoriteView.as_view(), name="add_favorite"),
     path("favorite-list/", FavoriteListView.as_view(), name="favorite_list"),
     path("remove-favorite/<int:product_id>/", remove_favorite, name="remove_favorite"),
-    path(
-        "products_list/price_asc/",
-        ProductAllListView.as_view(),
-        {"sort_by": "price_asc"},
-        name="products_list_price_asc",
-    ),
-    path(
-        "products_list/price_desc/",
-        ProductAllListView.as_view(),
-        {"sort_by": "price_desc"},
-        name="products_list_price_desc",
-    ),
     path(
         "products_all_list/price_asc/",
         ProductAllListView.as_view(),
